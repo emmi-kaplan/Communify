7c6ea74920918761be4328098601f97d029defb7
emmikaplan <emmiakaplan@gmail.com>
2024-01-26 11:34:26 -0800

    add reaction validation

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
+from sqlalchemy.orm import validates
 from datetime import datetime
 
 db = SQLAlchemy()
@@ -9,7 +11,8 @@ followers = db.Table(
     db.Column('followed_id', db.Integer, db.ForeignKey('profile_model.id'), primary_key=True)
 )
 
-class ProfileModel(db.Model):
+
+class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
@@ -23,7 +26,7 @@ class ProfileModel(db.Model):
     )
 
 
-class PostModel(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image_url = db.Column(db.String(255), nullable=False)
     body = db.Column(db.Text, nullable=False)
@@ -32,14 +35,23 @@ class PostModel(db.Model):
     time_stamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
-class ReactionModel(db.Model):
-    # this should be an enum
-    emotion = ['like', 'love', 'hate', 'uncomfortable', 'boring']
-    # is this the best way to have a reaction be either comment or emotion
-    # should I add validation so you can't react with no comment or emotion
+class Reaction(db.Model):
+    class Emotion(str, Enum):
+        like = 'like'
+        love = 'love'
+        hate = 'hate'
+        uncomfortable = 'uncomfortable'
+        boring = 'boring'
     comment = db.Column(db.Text, nullable=True)
-    emote = db.Column(emotion, nullable=True)
+    emote = db.Column(Emotion, nullable=True)
     author_id = db.Column(db.Integer, db.ForeignKey('profile_model.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post_model.id'), nullable=False)
     time_stamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # in order to react, you must either comment or emote
+    @validates('comment', 'emote')
+    def validate_comment_or_emote(self, key, value):
+        if self.emote is None and self.comment is None:
+            raise ValueError("Either 'comment' or 'emote' must be provided.")
+        return value
 
